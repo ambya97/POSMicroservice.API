bc657534023756cc162a73113b981bdde4771cd0
ambya97 <aviamkar23@gmail.com>
2024-06-24 21:44:44 +0530

    add repo code

--- POS.API/Program.cs
+++ POS.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddTransient<IDbConnection>(sp => new SqlConnection(builder.Configuration.GetConnectionString("defaultcon")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ POS.Core/Models/UnitModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Core.Models
+{
+    public class UnitModel
+    {
+        public int UnitId { get; set; }
+        public int UnitName { get; set; }
+    }
+}
