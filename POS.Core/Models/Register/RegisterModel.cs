467a72f98580fd42d25bffa3920c08e2b17838fe
ambya97 <aviamkar23@gmail.com>
2024-07-03 19:52:31 +0530

    save changes

---
+++ POS.Core/Models/Register/RegisterModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Core.Models.Register
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "FirstName is required")]
+        public string FirstName { get; set; } =string.Empty;
+
+        [Required(ErrorMessage = "LastName is required")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
