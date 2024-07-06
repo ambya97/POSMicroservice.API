cb63dc82ec32de54ba4415535b89928555cad78c
ambya97 <aviamkar23@gmail.com>
2024-07-06 19:54:42 +0530

    save changes

---
+++ POS.Core/Models/UserLogins/UserLoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Core.Models.UserLogins
+{
+    public class UserLoginModel
+    {
+        public int UserLoginId { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
---
+++ POS.Core/Models/UserToken/UserTokenModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Core.Models.UserToken
+{
+    public class UserTokenModel
+    {
+        public int UserTokenID { get; set; }
+        public string TokenValue  { get; set; } =string.Empty;
+        public int UserId { get; set; }
+    }
+}
